e002d678f853cecf28c91112d8531fe5fbed07bf
SanghyunLee <lgw9736@naver.com>
2024-11-26 19:07:12 +0900

    feat: WebsocketManager 생성
    - 소켓 연결 관련 추상화 클래스 생성
    - 싱글톤 형태

---
+++ src/services/WebSocketManager.ts
@@ -0,0 +1,54 @@
+import { Client, IMessage, IFrame } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+export abstract class WebSocketManager {
+  private static instance: WebSocketManager | null = null;
+  private client: Client | null = null;
+
+  public static getInstance<T extends WebSocketManager>(type: new () => T): T {
+    if (!WebSocketManager.instance) {
+      WebSocketManager.instance = new type();
+    }
+    return WebSocketManager.instance as T;
+  }
+
+  // 웹 소켓 연결
+  connect(): void {
+    this.client = new Client({
+      webSocketFactory: () => new SockJS(process.env.REACT_APP_WEBSOCKET_URL),
+      debug: (str) => console.log(`[STOMP Debug] ${str}`), // 후에 삭제
+      reconnectDelay: 5000,
+      onConnect: this.onConnect.bind(this),
+      onStompError: this.onError.bind(this),
+      onDisconnect: this.onDisconnect.bind(this),
+    });
+
+    this.client.activate();
+  }
+
+  abstract onConnect(frame: IFrame): void;
+  abstract onDisconnect(): void;
+  abstract onError(frame: IFrame): void;
+
+  // 웹 소켓 연결 종료
+  disconnect(): void {
+    if (this.client) {
+      this.client.deactivate();
+      this.client = null;
+    }
+  }
+
+  // stomp 프로토콜 구독
+  subscribe(destination: string, callback: (message: any) => void): void {
+    if (!this.client || !this.client.connected) {
+      return;
+    }
+
+    this.client.subscribe(destination, (message: IMessage) => {
+      const parsedBody = JSON.parse(message.body);
+      callback(parsedBody);
+    });
+
+    console.log(`Subscribed to ${destination}`); // 후에 삭제
+  }
+}
