1132c1b11119c5e96711b7217a83f2c9630f7301
jihukimme <jihu0210@naver.com>
2024-11-26 21:25:32 +0900

    fix: button, home, game 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,27 @@
-import Button from '../../components/button/Button';
+import Button, { ButtonType } from '../../components/button/Button';
 import * as styled from './Home.style';
 
 export default function Home() {
+  const roomId = '1234';
+
+  type ButtonProps = {
+    text: string;
+    type: ButtonType;
+    link: string;
+    onClick: () => void;
+  };
+
+  const buttonProps: ButtonProps = {
+    text: 'Game Start',
+    type: 'start',
+    link: `/game/${roomId}`,
+    onClick: () => console.log('Navigating to Game'),
+  };
+
   return (
     <styled.HomeWrapper>
       <h1>Home</h1>
-      <Button text="Game Start" />
+      <Button {...buttonProps} />
     </styled.HomeWrapper>
   );
 }
