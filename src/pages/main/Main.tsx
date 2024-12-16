cf4dbb7cd5a3cf1102d9e792f22d3f340a0a10a6
changuii <rhljh7410@gmail.com>
2024-12-16 18:00:03 +0900

    feat : 메인 페이지 구현 완료

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom';
+
+const Main = () => {
+
+    // 이스터에그
+    const easterEgg = `
+    _    _        _                               
+    | |  | |      | |                              
+    | |  | |  ___ | |  ___   ___   _ __ ___    ___ 
+    | |/\| | / _ \| | / __| / _ \ | '_ \` _ \  / _ \\
+    \\  /\\  /|  __/| || (__ | (_) || | | | | ||  __/
+    \\/  \\/  \\___||_| \\___| \\___/ |_| |_| |_| \\___|
+    _____  _  _        _     ___  ___                   _  _  _ 
+    /  __ \\| |(_)      | |    |  \\/  |                  | || || |
+    | /  \\/| | _   ___ | | __ | .  . |  ___   ___   ___ | || || |
+    | |    | || | / __|| |/ / | |\\/| | / _ \\ / _ \\ / _ \\| || || |
+    | \\__/\\| || || (__ |   <  | |  | ||  __/|  __/|  __/|_||_||_|
+    \\____/|_||_| \\___||_|\\_\\ \\_|  |_\\/ \\___| \\___| \\___|(_)(_)(_)
+`;
+
+console.log(easterEgg);
+
+
+    const navigate = useNavigate();
+
+    const handleNavigatePage = (path: string) => {
+        navigate(path);
+    }
+
+    return (
+        <>
+            <div className="flex z-10 flex-col justify-center items-center mt-10 md-10 bg-slate-50 bg-opacity-0 text-white p-6">
+                <div className="bg-gray-700 rounded-xl max-w-100 w-2/5 min-w-80 h-5/6 p-10 shadow-floating">
+                    <div className="text-center text-3xl mb-10">🐹 Welcome Click Meee!!! 🐭</div>
+                    <button onClick={() => handleNavigatePage('')} className="basic-button text-xl text-center mb-5">📒 방 목록</button>
+                    <button onClick={() => handleNavigatePage('/home')} className="basic-button text-xl text-center mb-5">🎊 방 생성</button>
+                    <button onClick={() => handleNavigatePage('')} className="basic-button text-xl text-center mb-5">🚪 방 코드 입장</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Main;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,12 +2,14 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import GameReady from '../pages/game-ready/GameReady.tsx';
 import Home from '../pages/home/Home';
 import Game from "../pages/game/Game.tsx";
+import Main from "../pages/main/Main.tsx"
 
 export default function Router() {
   return (
     <Routes>
       {/* 진입 지점 */}
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/game-ready/:roomId" element={<GameReady />} />
       <Route path="/game/:roomId" element={<Game />} />
       {/* default 경로 설정 */}
