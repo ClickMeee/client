7534cca98e9b4362c3911d1127f7ec16b841d118
changuii <rhljh7410@gmail.com>
2024-12-18 12:00:21 +0900

    feat : 게임 시작 전 카운트다운 4초 (게임 시작 메시지 포함)을 만들어라

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,44 +1,81 @@
-import { oneVsOneWebSocket } from "../../services/OneVsOneWebSocket";
-import TeamChart from "../../components/chart/TeamChart";
-import IndividualChart from "../../components/chart/IndividualChart";
-import TeamRank from "../../components/teamRank/TeamRank";
-
+import { oneVsOneWebSocket } from '../../services/OneVsOneWebSocket';
+import TeamChart from '../../components/chart/TeamChart';
+import IndividualChart from '../../components/chart/IndividualChart';
+import TeamRank from '../../components/teamRank/TeamRank';
+import { useEffect, useState } from 'react';
 
 const Game = () => {
+  const [count, setCount] = useState<number>(4);
+  const [moveMessage, setMoveMessage] = useState<boolean>(false);
+
+
+  const startMessage = '게임 시작'
+  const second = 1000;
+  const halfSecond = 500;
+  useEffect(() => {
+    setMoveMessage(true)
+    const handleCountdown = () => {
+      setCount(prevCount => {
+        setMoveMessage(true);
+        if(prevCount == 1){
+          clearInterval(interval);
+        }
+        return prevCount - 1;
+      });
+    };
+
+    const handleMove = () => {
+      setMoveMessage(false);
+    }
+
+    let interval = setInterval(handleCountdown, second);
+    let i = setInterval(handleMove, halfSecond);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const handleButtonClick = () => {
     oneVsOneWebSocket.sendClickEvent();
   };
 
   return (
-    <div className='flex flex-col gap-8 p-8 h-full'>
-      <div
-        className="flex flex-col items-center justify-center w-full h-2/3 p-4 bg-white rounded-xl shadow-xl box-border">
-        {/* 차트 표시 div */}
-        <div className="flex gap-4 w-full h-full">
-          <div className="flex-[3] h-full">
-            <TeamChart />
-          </div>
-          <div className="flex flex-col flex-[2] gap-4">
-            <div className="flex-1 h-1/2">
-              <IndividualChart />
+    <>
+      {count > 0 ? (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-gray-500 bg-opacity-50"></div>
+          <div className={`transform text-9xl text-red-500 relative ease-in-out transition-all ${moveMessage ? '-translate-y-20' : ''} duration-1000`}>{count > 1 ? count-1 : startMessage}</div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="flex flex-col gap-8 p-8 h-full">
+        <div className="flex flex-col items-center justify-center w-full h-2/3 p-4 bg-white rounded-xl shadow-xl box-border">
+          {/* 차트 표시 div */}
+          <div className="flex gap-4 w-full h-full">
+            <div className="flex-[3] h-full">
+              <TeamChart />
             </div>
-            <div className="flex-1 h-1/2 p-4">
-              <TeamRank />
+            <div className="flex flex-col flex-[2] gap-4">
+              <div className="flex-1 h-1/2">
+                <IndividualChart />
+              </div>
+              <div className="flex-1 h-1/2 p-4">
+                <TeamRank />
+              </div>
             </div>
           </div>
         </div>
+        {/* 클릭 버튼 */}
+        <div className="flex-1 flex w-full h-full box-border shadow-xl">
+          <button
+            className="w-full h-full bg-orange-400 text-xl font-bold rounded-lg hover:bg-orange-500 box-border"
+            onClick={handleButtonClick}
+          >
+            Click Mee!
+          </button>
+        </div>
       </div>
-      {/* 클릭 버튼 */}
-      <div className="flex-1 flex w-full h-full box-border shadow-xl">
-        <button
-          className="w-full h-full bg-orange-400 text-xl font-bold rounded-lg hover:bg-orange-500 box-border"
-          onClick={handleButtonClick}
-        >
-          Click Mee!
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
