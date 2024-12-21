c886ced5a06b0d87ff681bb2695d962f8c8f1dd5
SanghyunLee <lgw9736@naver.com>
2024-12-21 21:43:07 +0900

    chore: StrictMode 주석 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  // TODO: 개발 끝나면 StrictMode 다시 활성화하기
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>
+  </StrictMode>
 );
