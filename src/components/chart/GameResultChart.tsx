2fe38f01621c59887301a641954bc8e3694c8df5
SanghyunLee <lgw9736@naver.com>
2024-12-22 14:15:49 +0900

    refactor: 폭죽 효과 hook으로 분리

--- src/components/chart/GameResultChart.tsx
+++ src/components/chart/GameResultChart.tsx
@@ -9,42 +9,10 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import confetti from 'canvas-confetti';
+import useFirework from "../../hooks/useFirework.ts";
 
 // Chart.js 모듈 등록
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-
-function firework() {
-  const duration = 5000;
-  const animationEnd = Date.now() + duration;
-  const defaults = { startVelocity: 25, spread: 360, ticks: 50, zIndex: 100 };
-
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  let interval = setInterval(function () {
-    let timeLeft = animationEnd - Date.now();
-
-    if (timeLeft <= 0) {
-      return clearInterval(interval);
-    }
-
-    let particleCount = 50 * (timeLeft / duration);
-    confetti(
-      Object.assign({}, defaults, {
-        particleCount,
-        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
-      })
-    );
-    confetti(
-      Object.assign({}, defaults, {
-        particleCount,
-        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
-      })
-    );
-  }, 250);
-}
 
 const GameResultChart: React.FC = () => {
   const [isChartReached, setIsChartReached] = useState<boolean>(false);
@@ -100,7 +68,7 @@ const GameResultChart: React.FC = () => {
 
     if (allReached && !isChartReached) {
       setIsChartReached(true);
-      firework(); // firework 실행
+      useFirework(); // firework 실행
     }
   }, [currentScores, finalScores, isChartReached]);
 
@@ -148,7 +116,7 @@ const GameResultChart: React.FC = () => {
 
   return (
     <div className="relative w-1/4 h-96">
-      {isChartReached && firework()} {/* 상태에 따라 firework 실행 */}
+      {isChartReached && useFirework()}
       <Bar ref={chartRef} data={chartData} options={chartOptions} />
       {isChartReached && (
         <div className="fixed inset-0 flex items-center justify-center z-30">
---
+++ src/hooks/useFirework.ts
@@ -0,0 +1,35 @@
+import confetti from 'canvas-confetti';
+
+const useFirework= () => {
+  const duration = 5000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 25, spread: 360, ticks: 50, zIndex: 100 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  let interval = setInterval(function () {
+    let timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    let particleCount = 50 * (timeLeft / duration);
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+      })
+    );
+  }, 250);
+}
+
+export default useFirework;
