eb55e9e19c1e8a8b6f8a0551e6372cc843465f1a
jihukimme <jihu0210@naver.com>
2025-01-10 11:24:16 +0900

    chore: DetailRank 리팩토링링

--- src/pages/game-result/GameResult.tsx
+++ src/pages/game-result/GameResult.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
+import GameResultChart from '../../components/chart/GameResultChart.tsx';
 import MessageModal from '../../components/modal/MessageModal.tsx';
+import DetailRank from '../../components/rank/DetailRank.tsx';
 import useMessages from '../../hooks/useMessage.ts';
 import { gameReadyState } from '../../recoil/atoms/gameReadyState';
 import { gameState } from '../../recoil/atoms/gameState.ts';
@@ -10,7 +11,6 @@ import WebSocketManager from '../../services/WebSocketManager.ts';
 import { GameStateDataProps } from '../../types/GameStateData.type.ts';
 import { RoomClientProps } from '../../types/RoomClient.type.ts';
 import { RoomDataProps } from '../../types/RoomData.type.ts';
-import GameResultChart from '../../components/chart/GameResultChart.tsx';
 
 export default function GameResult() {
   const webSocketManager = WebSocketManager.getInstance();
@@ -26,8 +26,9 @@ export default function GameResult() {
     <>
       <MessageModal messages={messages} />
       <div className="w-full h-full justify-center flex items-center">
-        <div className="bg-white rounded-3xl shadow-2xl w-10/12 h-5/6 flex justify-center overflow-hidden">
+        <div className="bg-white rounded-3xl shadow-2xl w-10/12 flex flex-col justify-center overflow-hidden">
           <GameResultChart />
+          <DetailRank />
         </div>
       </div>
     </>
